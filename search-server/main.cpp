76490903cf911266a1b82a97426ca069bc083517
Evgenicast <107400788+Evgenicast@users.noreply.github.com>
2022-08-16 00:48:57 +0400

    Changes were made to Initial commit according to reviewer's suggestions and corrections

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -83,7 +83,7 @@ enum class DocumentStatus
     BANNED,
     REMOVED,
 };
-//    inline static constexpr int INVALID_DOCUMENT_ID = -1;
+
 class SearchServer
 {
 public:
@@ -92,81 +92,58 @@ public:
     explicit SearchServer(const StringContainer& stop_words)
         : stop_words_(MakeUniqueNonEmptyStrings(stop_words))
         {
-            for ( const auto& word : stop_words )
+            for (const auto& word : stop_words)
             {
-                if ( !IsValidSymbol(word) )
-                throw invalid_argument( "Invalid word format or forbidden symbols have been added" );
+                if (!IsValidSymbol(word))
+                {
+                    throw invalid_argument( "Invalid word format or forbidden symbols have been added"s );
+                }
             }
         }
 
     explicit SearchServer(const string& stop_words_text)
         : SearchServer(SplitIntoWords(stop_words_text))
         {
-            if ( !IsValidSymbol(stop_words_text) )
-            throw invalid_argument( "Invalid word format or forbidden symbols have been added" );
         }
 
-    bool AddDocument(int document_id,
+    void AddDocument(int document_id,
                     const string& document,
                     DocumentStatus status,
                     const vector<int>& ratings)
     {
-        if ( (document_id < 0) || (documents_.count(document_id) > 0) )
+        if (document_id < 0)
         {
-            throw invalid_argument( "Document id is invalid" );
+            throw invalid_argument( "Document id "s + to_string(document_id) + " is invalid (is negative)"s );
         }
 
-        vector<string> words;
-        if ( !SplitIntoWordsNoStop(document, words) )
+        if (documents_.count(document_id) > 0)
         {
-            throw invalid_argument( "Invalid word format or forbidden symbols have been added" );
-        }
-        for ( const auto& word : words )
-        {
-            if ( !IsValidSymbol(word) )
-            throw invalid_argument( "Invalid word format or forbidden symbols have been added" );
+            throw invalid_argument( "Document with such ID already exists"s );
         }
 
+        vector<string> words =  SplitIntoWordsNoStop(document);
         const double inv_word_count = 1.0 / words.size();
-        for ( const string& word : words )
+        for (const string& word : words)
         {
             word_to_document_freqs_[word][document_id] += inv_word_count;
         }
         documents_.emplace(document_id, DocumentData{ComputeAverageRating(ratings), status});
         document_ids_.push_back(document_id);
-        return true;
     }
 
     template <typename DocumentPredicate>
     vector<Document> FindTopDocuments(const string& raw_query, DocumentPredicate document_predicate) const
     {
         const Query query = ParseQuery(raw_query);
-
-        //My solution for the Ivnvalid query. We have minus_words, plus_words and from now on - invalid_words.
-        if ( query.invalid_words.size() != 0 )
-        {
-            throw invalid_argument( "Invalid word format or forbidden symbols have been parsed" );
-        }
-        //-----   another variant for InValidWordcheck (for ex."--")  ----//
-        //vector<string> words;
-        //if (!SplitIntoWordsNoStop(raw_query, words))
-        //{
-        //    throw invalid_argument("Invalid word format or forbidden symbols have been parsed");
-        //}
-        if ( !IsValidSymbol(raw_query) )
-        {
-            throw invalid_argument( "Invalid word format or forbidden symbols have been added" );
-        }
-        if ( !IsSpaceAfterMinus(raw_query))
+        if (!IsSpaceAfterMinus(raw_query))
          {
-            throw invalid_argument( "Extra space after minus is not allowed" );
+            throw invalid_argument( "Extra space after minus is not allowed"s );
          }
 
         auto matched_documents = FindAllDocuments(query, document_predicate);
-
         sort(matched_documents.begin(), matched_documents.end(), [](const Document& lhs, const Document& rhs)
         {
-            if ( abs(lhs.relevance - rhs.relevance ) < EPSILON)
+            if (abs(lhs.relevance - rhs.relevance) < EPSILON)
             {
                 return lhs.rating > rhs.rating;
             }
@@ -175,13 +152,11 @@ public:
                 return lhs.relevance > rhs.relevance;
             }
         });
-        if ( matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT )
+        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT)
         {
             matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
         }
-
-         return matched_documents;
-
+        return matched_documents;
     }
 
     vector<Document> FindTopDocuments(const string& raw_query, DocumentStatus status) const
@@ -207,57 +182,40 @@ public:
         return document_ids_.at(index);
     }
 
-//    int GetDocumentId(int index) const
-//    {
-//        if (index >= 0 && index < GetDocumentCount())
-//        {
-//            return document_ids_[index];
-//        }
-//        return INVALID_DOCUMENT_ID;
-//    }
-
     tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query, int document_id) const
     {
         Query query = ParseQuery((raw_query));
         vector<string> matched_words;
         optional <tuple<vector<string>, DocumentStatus>> temp;
-        if ( query.invalid_words.size() != 0 )
+        if (!IsSpaceAfterMinus(raw_query))
         {
-            throw invalid_argument("Invalid word format or forbidden symbols have been parsed");
-        }
-        if ( !IsValidSymbol(raw_query) )
-        {
-            throw invalid_argument("Invalid word format or forbidden symbols have been added");
-        }
-        if ( !IsSpaceAfterMinus(raw_query) )
-        {
-            throw invalid_argument("Extra space after minus is not allowed");
+            throw invalid_argument( "Extra space after minus is not allowed"s );
         }
 
-        for ( const string& word : query.plus_words )
+        for (const string& word : query.plus_words)
         {
-            if ( word_to_document_freqs_.count(word) == 0 )
+            if (word_to_document_freqs_.count(word) == 0)
             {
                 continue;
             }
-            if ( word_to_document_freqs_.at(word).count(document_id) )
+            if (word_to_document_freqs_.at(word).count(document_id))
             {
                 matched_words.push_back(word);
             }
         }
-        for ( const string& word : query.minus_words )
+
+        for (const string& word : query.minus_words)
         {
-            if ( word_to_document_freqs_.count(word) == 0 )
+            if (word_to_document_freqs_.count(word) == 0)
             {
                 continue;
             }
-            if ( word_to_document_freqs_.at(word).count(document_id) )
+            if (word_to_document_freqs_.at(word).count(document_id))
             {
                 matched_words.clear();
                 break;
             }
         }
-
         return  {matched_words, documents_.at(document_id).status};
     }
 
@@ -280,10 +238,9 @@ private:
     bool IsSpaceAfterMinus(const string& text) const
     {
         bool minus = false;
-
         return !(text.back() == '-');
 
-        for ( char c : text )
+        for (char c : text)
         {
              return !((c == '-') ? (minus = true) : (c == ' ') && (minus == true));
         }
@@ -306,28 +263,30 @@ private:
         });
     }
 
-    bool SplitIntoWordsNoStop(const string& text, vector<string>& result) const
+    vector<string> SplitIntoWordsNoStop(const string& text) const
     {
-        result.clear();
         vector<string> words;
-        for ( const string& word : SplitIntoWords(text) )
+        for (const string& word : SplitIntoWords(text))
         {
-            if ( !IsValidWord(word) )
+            if (!IsValidWord(word))
             {
-                return false;
+                throw invalid_argument( "Word "s + word + " is invalid"s );
             }
-            if ( !IsStopWord(word) )
+            if (!IsValidSymbol(word))
+            {
+                throw invalid_argument( "Invalid word format or forbidden symbols"s + word + "have been added"s );
+            }
+            if (!IsStopWord(word))
             {
                 words.push_back(word);
             }
         }
-        result.swap(words);
-        return true;
+        return words;
     }
 
     static int ComputeAverageRating(const vector<int>& ratings)
     {
-        if ( ratings.empty() )
+        if (ratings.empty())
         {
             return 0;
         }
@@ -341,15 +300,23 @@ private:
         bool is_minus;
         bool is_stop;
     };
+
     QueryWord ParseQueryWord(string text) const
     {
         bool is_minus = false;
-        if ( text[0] == '-' )
+        if (text[0] == '-')
         {
             is_minus = true;
             text = text.substr(1);
         }
-
+        if (text[0] == '-')
+        {
+            throw invalid_argument( "Parsing query "s + text + " is invalid (extra minus in front)"s );
+        }
+        if (!IsValidSymbol(text))
+        {
+            throw invalid_argument( "Query "s + text + " is invalid (contains forbidden symbols)"s );
+        }
         return QueryWord{text, is_minus, IsStopWord(text)};
     }
 
@@ -357,21 +324,17 @@ private:
     {
         set<string> plus_words;
         set<string> minus_words;
-        vector<string> invalid_words;
     };
+
     Query ParseQuery(const string& text) const
     {
         Query query;
-        for ( const string& word : SplitIntoWords(text) )
+        for (const string& word : SplitIntoWords(text))
         {
             const QueryWord query_word = ParseQueryWord(word);
-            if ( query_word.data[0] == '-' )
+            if (!query_word.is_stop)
             {
-                query.invalid_words.push_back(query_word.data);
-            }
-            if ( !query_word.is_stop )
-            {
-                if ( query_word.is_minus )
+                if (query_word.is_minus)
                 {
                     query.minus_words.insert(query_word.data);
                 }
@@ -381,7 +344,7 @@ private:
                 }
             }
         }
-     return query;
+        return query;
     }
 
     double ComputeWordInverseDocumentFreq(const string& word) const
@@ -393,29 +356,30 @@ private:
     vector<Document> FindAllDocuments(const Query& query, DocumentPredicate document_predicate) const
     {
         map<int, double> document_to_relevance;
-        for ( const string& word : query.plus_words )
+        for (const string& word : query.plus_words)
         {
-            if ( word_to_document_freqs_.count(word) == 0 )
+            if (word_to_document_freqs_.count(word) == 0)
             {
                 continue;
             }
             const double inverse_document_freq = ComputeWordInverseDocumentFreq(word);
-            for ( const auto [document_id, term_freq] : word_to_document_freqs_.at(word) )
+            for (const auto [document_id, term_freq] : word_to_document_freqs_.at(word))
             {
                 const auto& document_data = documents_.at(document_id);
-                if ( document_predicate(document_id, document_data.status, document_data.rating) )
+                if (document_predicate(document_id, document_data.status, document_data.rating))
                 {
                     document_to_relevance[document_id] += term_freq * inverse_document_freq;
                 }
             }
         }
-        for ( const string& word : query.minus_words )
+
+        for (const string& word : query.minus_words)
         {
-            if ( word_to_document_freqs_.count(word) == 0 )
+            if (word_to_document_freqs_.count(word) == 0)
             {
                 continue;
             }
-            for ( const auto [document_id, _] : word_to_document_freqs_.at(word) )
+            for (const auto [document_id, _] : word_to_document_freqs_.at(word))
             {
                 document_to_relevance.erase(document_id);
             }
